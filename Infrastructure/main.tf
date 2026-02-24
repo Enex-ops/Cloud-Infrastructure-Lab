4011ec3dede47da3a1a2b612b1488333f174065d
Enex-ops <campage1999@gmail.com>
2026-02-24 22:02:22 +1030

    Remove S3 bucket ACL resource
    Delete the aws_s3_bucket_acl resource from Infrastructure/main.tf and replace it with a comment explaining why

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -7,10 +7,12 @@ resource "aws_s3_bucket" "frontend_bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "frontend_bucket_acl" {
-  bucket = aws_s3_bucket.frontend_bucket.id
-  acl    = "private" 
-}
+/*
+ The bucket does not allow ACLs (AccessControlListNotSupported).
+ ACLs are disabled by the account/bucket (Object Ownership = BucketOwnerEnforced).
+ Access should be managed with a bucket policy or IAM permissions instead.
+ Removed the aws_s3_bucket_acl resource to avoid API errors.
+ */
 
 resource "random_id" "bucket_name_suffix" {
   byte_length = 4
