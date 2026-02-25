5b9f1941b04153975d4538ffe41df5c973242077
Enex-ops <campage1999@gmail.com>
2026-02-25 18:54:45 +1030

    Adding Deletion Protection for Lock Table
    In the resourse code for the lock table. Added an additional line to enable deletion protection for the remote state.

--- Terraform Bootstrap/Terraform Code/main.tf
+++ Terraform Bootstrap/Terraform Code/main.tf
@@ -49,6 +49,7 @@ resource "aws_dynamodb_table" "terraform_locks" {
     name           = "terraform-locks"
     billing_mode   = "PAY_PER_REQUEST"
     hash_key       = "LockID"
+    deletion_protection_enabled = true
 
     attribute {
         name = "LockID"
