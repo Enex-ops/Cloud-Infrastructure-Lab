b770a7d3e4b70d2009d84165833de7b4b96e22b9
Enex-ops <campage1999@gmail.com>
2026-03-12 23:41:52 +1030

    Update backend.tf

--- Infrastructure/backend.tf
+++ Infrastructure/backend.tf
@@ -8,36 +8,3 @@ terraform {
   }
 }
 
-resource "aws_vpc" "lab-vpc" {
-  cidr_block = "10.0.0.0/16"
-  
-  tags = {
-    Name        = "lab VPC"
-    Environment = "Production"
-  }
-}
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow-tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.lab-vpc.id
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name        = "allow-tls"
-    Environment = "Production"
-  }
-}
