6fd60abfead5a4a8a6452e647061244a5bec9a56
Cameron <campage1999@gmail.com>
2026-03-12 21:53:53 +1030

    Update backend.tf

--- Infrastructure/backend.tf
+++ Infrastructure/backend.tf
@@ -5,6 +5,16 @@ terraform {
     region         = "ap-southeast-2"
     encrypt        = true
     use_lockfile = "true"
-    
   }
+}
+
+resource "aws_vpc" "lab-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name        = "lab VPC"
+    Environment = "Production"
+  }
+
+  
 }
