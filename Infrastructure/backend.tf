39aa89ff7fcd20ab2eceefed43fb5e8cee3eedbd
Cameron <campage1999@gmail.com>
2026-03-13 15:43:49 +1030

    Formatting

--- Infrastructure/backend.tf
+++ Infrastructure/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-145023112872"
-    key            = "global/s3/terraform.tfstate"
-    region         = "ap-southeast-2"
-    encrypt        = true
+    bucket       = "terraform-state-145023112872"
+    key          = "global/s3/terraform.tfstate"
+    region       = "ap-southeast-2"
+    encrypt      = true
     use_lockfile = true
   }
 }
