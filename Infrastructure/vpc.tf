f54d32020a7e273dd1caacc207b3a69222e672aa
Enex-ops <campage1999@gmail.com>
2026-03-13 23:30:32 +1030

    Added commenting to vpc.tf

--- Infrastructure/vpc.tf
+++ Infrastructure/vpc.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "lab-vpc" {
+resource "aws_vpc" "lab-vpc" { // Define a VPC resource named "lab-vpc"
   cidr_block = "10.0.0.0/16"
 
   tags = {
@@ -7,26 +7,26 @@ resource "aws_vpc" "lab-vpc" {
   }
 }
 
-resource "aws_security_group" "allow_tls" {
+
+resource "aws_security_group" "allow_tls" { // Create a security group that allows inbound TLS traffic
   name        = "allow-tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.lab-vpc.id
-
-  ingress {
+  vpc_id      = aws_vpc.lab-vpc.id 
+  ingress { // Allow inbound traffic on port 443 (HTTPS) from any source
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
+  egress { // Allow all outbound traffic from the security group
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
+  tags = { // Tag the security group for identification
     Name        = "allow-tls"
     Environment = "Production"
   }
