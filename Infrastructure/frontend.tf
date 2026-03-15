f101e2df68c96cd137f6b0fb168127a7379dc4d7
Enex-ops <campage1999@gmail.com>
2026-03-15 22:13:17 +1030

    Commented out CloudFront code while created domain

--- Infrastructure/frontend.tf
+++ Infrastructure/frontend.tf
@@ -36,89 +36,89 @@ locals {
   staticweb_domain = "staticweb.com"
 }
 
-resource "aws_acm_certificate" "staticweb_cert" {
-  provider          = aws.us_east_1
-  validation_method = "DNS"
-  domain_name       = local.staticweb_domain
+# resource "aws_acm_certificate" "staticweb_cert" {
+#   provider          = aws.us_east_1
+#   validation_method = "DNS"
+#   domain_name       = local.staticweb_domain
 
-  tags = {
-    Name        = "lab Static Web Certificate"
-    Environment = "Production"
-  }
-}
+#   tags = {
+#     Name        = "lab Static Web Certificate"
+#     Environment = "Production"
+#   }
+# }
 
-data "aws_acm_certificate" "staticweb_cert" {
-  provider = aws.us_east_1
-  domain   = local.staticweb_domain
-  statuses = ["ISSUED"]
-}
+# data "aws_acm_certificate" "staticweb_cert" {
+#   provider = aws.us_east_1
+#   domain   = local.staticweb_domain
+#   statuses = ["ISSUED"]
+# }
 
-resource "aws_cloudfront_origin_access_identity" "oai" {
-  comment = "OAI for CloudFront to access S3 bucket"
+# resource "aws_cloudfront_origin_access_identity" "oai" {
+#   comment = "OAI for CloudFront to access S3 bucket"
 
-}
+# }
 
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name = aws_s3_bucket.staticweb_bucket.bucket_regional_domain_name
-    origin_id   = local.s3_origin_id
+# resource "aws_cloudfront_distribution" "s3_distribution" {
+#   origin {
+#     domain_name = aws_s3_bucket.staticweb_bucket.bucket_regional_domain_name
+#     origin_id   = local.s3_origin_id
 
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
-    }
-  }
+#     s3_origin_config {
+#       origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+#     }
+#   }
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = "CloudFront Distribution for S3 Static Website"
-  default_root_object = "index.html"
+#   enabled             = true
+#   is_ipv6_enabled     = true
+#   comment             = "CloudFront Distribution for S3 Static Website"
+#   default_root_object = "index.html"
 
-  default_cache_behavior {
-    target_origin_id       = local.s3_origin_id
-    viewer_protocol_policy = "redirect-to-https"
+#   default_cache_behavior {
+#     target_origin_id       = local.s3_origin_id
+#     viewer_protocol_policy = "redirect-to-https"
 
-    allowed_methods = ["GET", "HEAD", "OPTIONS"]
-    cached_methods  = ["GET", "HEAD"]
+#     allowed_methods = ["GET", "HEAD", "OPTIONS"]
+#     cached_methods  = ["GET", "HEAD"]
 
-    forwarded_values {
-      query_string = false
+#     forwarded_values {
+#       query_string = false
 
-      cookies {
-        forward = "none"
-      }
-    }
-  }
+#       cookies {
+#         forward = "none"
+#       }
+#     }
+#   }
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "whitelist"
-      locations        = ["AU", "US"]
-    }
-  }
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "whitelist"
+#       locations        = ["AU", "US"]
+#     }
+#   }
 
-  viewer_certificate {
-    acm_certificate_arn      = data.aws_acm_certificate.staticweb_cert.arn
-    ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2021"
-  }
-}
+#   viewer_certificate {
+#     acm_certificate_arn      = data.aws_acm_certificate.staticweb_cert.arn
+#     ssl_support_method       = "sni-only"
+#     minimum_protocol_version = "TLSv1.2_2021"
+#   }
+# }
 
-// data "aws_route53_zone" "staticweb_zone" {
-//name         = local.staticweb_domain
-//  private_zone = false
-// }
+#  data "aws_route53_zone" "staticweb_zone" {
+# name         = local.staticweb_domain
+#   private_zone = false
+#  }
 
-//resource "aws_route53_record" "staticweb_record" {
-//for_each = aws_cloudfront_distribution.s3_distribution.aliases
-// zone_id = data.aws_route53_zone.staticweb_zone.zone_id
-// name    = local.staticweb_domain
-//  type    = "CNAME"
-//  ttl     = 300
-//   records = [aws_cloudfront_distribution.s3_distribution.domain_name]
+# resource "aws_route53_record" "staticweb_record" {
+# for_each = aws_cloudfront_distribution.s3_distribution.aliases
+#  zone_id = data.aws_route53_zone.staticweb_zone.zone_id
+#  name    = local.staticweb_domain
+#   type    = "CNAME"
+#   ttl     = 300
+#    records = [aws_cloudfront_distribution.s3_distribution.domain_name]
 
-// alias {
-//    name = aws_cloudfront_distribution.s3_distribution.domain_name
-//    zone_id = aws_cloudfront_distribution.s3_distribution_zone_id
-//    evaluate_target_health = false
-//   }
-// }
+#  alias {
+#     name = aws_cloudfront_distribution.s3_distribution.domain_name
+#     zone_id = aws_cloudfront_distribution.s3_distribution_zone_id
+#     evaluate_target_health = false
+#    }
+#  }
